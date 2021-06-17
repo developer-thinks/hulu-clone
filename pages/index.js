aec1b105d54568cda01d92d74fc1e798eb990967
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-06-17 22:19:21 +0530

    movie review app

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from './../components/Header';
+import Nav from '../components/Nav';
+import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home({results}) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -10,8 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h2> Lets build own social media for movies show and rating and etc etc etc .....</h2>
+      <Header />
+      <Nav />
+      <Results results={results} />
      
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json());
+
+  return {
+    props: {
+      results : request.results,
+    },
+  }
+
 }
